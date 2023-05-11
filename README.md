2cded559533401f19e74cef3f6ada7a60e775345
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-11 16:15:58 +1000

    Initial
    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+### CoreKeeper Ultra-wide Fix
+
+![Game Logo](header.jpg)<br>
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/F2F2DI3WA)<br>
+[![Github All Releases](https://img.shields.io/github/downloads/p1xel8ted/CoreKeeper/total.svg)](https://github.com/p1xel8ted/CoreKeeper/releases)
+
+## Notes
+
+- This uses a custom build of BepInEx from [here](https://core-keeper.thunderstore.io/package/BepInEx/BepInExPack_Core_Keeper/) should it need updating in the future.
+- The game is also an IL2CPP game, so first start (and after any game updates) will be a little longer while it generates assemblies.
+
+## Features
+
+- Removes pillar boxing in the menus.
+- Expands viewport width. Note you will see world spawn/de-spawning near the edges of the screen. Most likely be worse the wider the aspect ratio.
+- The viewport width is adjusted automatically, but a custom width can be set.
+- Skips intros.
+
+## Installation
+- Grab the latest release of CoreKeeper Ultra-wide Fix from [here.](https://github.com/p1xel8ted/CoreKeeper/releases)
+- Extract the contents of the release zip in to the game directory.<br />(e.g. "**steamapps\common\CoreKeeper**" for Steam).
---
+++ UltraWide/Plugin.cs
@@ -0,0 +1,99 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using PugRP;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace UltraWide;
+
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class Plugin : BasePlugin
+{
+    private const string PluginGuid = "p1xel8ted.corekeeper.ultrawide";
+    private const string PluginName = "CoreKeeper UltraWide";
+    private const string PluginVersion = "0.0.1";
+
+    private const string LeftPillarBoxName = "Left";
+    private const string RightPillarBoxName = "Right";
+    private static ConfigEntry<bool> UseCustomMaxWidth { get; set; }
+    private static ConfigEntry<int> CustomMaxWidth { get; set; }
+
+    public override void Load()
+    {
+        UseCustomMaxWidth = Config.Bind("General", "UseCustomMaxWidth", false, "At auto, you will see the world spawning/de-spawning. It's not so bad at 21:9, but will be worse at wider resolutions. Adjust this value to your liking.");
+        UseCustomMaxWidth.SettingChanged += (_, _) =>
+        {
+            if (UseCustomMaxWidth.Value)
+            {
+                UpdatePugCameras(true);
+            }
+            else
+            {
+                UpdatePugCameras();
+            }
+        };
+        CustomMaxWidth = Config.Bind("General", "MaxWidth", 480, "This is not the width in screen pixels. For example at 3440x1440 (21:9), a value of 630 expands the viewport entirely.");
+        CustomMaxWidth.SettingChanged += (_, _) =>
+        {
+            if (UseCustomMaxWidth.Value)
+            {
+                UpdatePugCameras(true);
+            }
+        };
+        SceneManager.sceneLoaded += (UnityAction<Scene, LoadSceneMode>) SceneManagerOnSceneLoaded;
+    }
+
+    public override bool Unload()
+    {
+        SceneManager.sceneLoaded -= (UnityAction<Scene, LoadSceneMode>) SceneManagerOnSceneLoaded;
+        return base.Unload();
+    }
+
+    private static void UpdatePugCameras(bool custom = false)
+    {
+        var pugCameras = Resources.FindObjectsOfTypeAll<PugCamera>();
+        foreach (var cam in pugCameras)
+        {
+            cam.m_outputWidth = custom ? CustomMaxWidth.Value : cam.m_maxOutputWidth;
+            if (cam.camera != null)
+            {
+                cam.camera.aspect = Display.main.systemWidth / (float) Display.main.systemHeight;
+            }
+        }
+    }
+
+    private static void RemovePillars()
+    {
+        var spr = Resources.FindObjectsOfTypeAll<SpriteRenderer>();
+        foreach (var s in spr)
+        {
+            if (s.name == LeftPillarBoxName)
+            {
+                s.gameObject.SetActive(false);
+            }
+
+            if (s.name == RightPillarBoxName)
+            {
+                s.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private static void UpdateCameras()
+    {
+        var cameras = Resources.FindObjectsOfTypeAll<Camera>();
+        foreach (var cam in cameras)
+        {
+            cam.aspect = Display.main.systemWidth / (float) Display.main.systemHeight;
+        }
+    }
+
+    private static void SceneManagerOnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        UpdateCameras();
+        UpdatePugCameras();
+        RemovePillars();
+    }
+}
