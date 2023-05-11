c177aba9153a88f678c800840a3d78ab969aa2b6
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-12 01:01:10 +1000

    Update CoreKeeper UltraWide plugin version to 0.0.2, add logging and options for custom max width.

--- UltraWide/Plugin.cs
+++ UltraWide/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using PugRP;
 using UnityEngine;
@@ -13,16 +14,60 @@ public class Plugin : BasePlugin
 {
     private const string PluginGuid = "p1xel8ted.corekeeper.ultrawide";
     private const string PluginName = "CoreKeeper UltraWide";
-    private const string PluginVersion = "0.0.1";
+    private const string PluginVersion = "0.0.2";
 
     private const string LeftPillarBoxName = "Left";
     private const string RightPillarBoxName = "Right";
     private static ConfigEntry<bool> UseCustomMaxWidth { get; set; }
     private static ConfigEntry<int> CustomMaxWidth { get; set; }
 
+    private static ManualLogSource Logger { get; set; }
+    private static Options Options { get; set; }
+    
+
+    private static int CalculateMaxWidth()
+    {
+        var aspect = Display.main.systemWidth / (float) Display.main.systemHeight;
+
+        // Define your reference points (16:9 and 3440x1440)
+        const float minAspect = 16f / 9f;  
+        const float refAspect = 3440f / 1440f; // Actual aspect ratio for 3440x1440
+
+        const float minWidth = 480;
+        const float refWidth = 648; // Corresponding width value for the reference aspect
+
+        float maxWidth;
+
+        // If the aspect is less than or equal to the reference aspect (3440x1440)
+        if (aspect <= refAspect)
+        {
+            // Interpolate between minWidth (480) and refWidth (648) based on the aspect ratio
+            var t = (aspect - minAspect) / (refAspect - minAspect);
+            maxWidth = minWidth + (refWidth - minWidth) * t;
+        }
+        else
+        {
+            // If the aspect is larger than the reference aspect, scale proportionally
+            maxWidth = refWidth * (aspect / refAspect);
+        }
+
+        // Increase by 2% and round
+        var roundedMaxWidth = Mathf.RoundToInt(maxWidth * 1.02f);
+
+        return roundedMaxWidth;
+    }
+
+
+
     public override void Load()
     {
+        Logger = Log;
+        Options = AddComponent<Options>();
+        var maxWidth = CalculateMaxWidth();
+        Options.SliderValueMax = maxWidth;
         UseCustomMaxWidth = Config.Bind("General", "UseCustomMaxWidth", false, "At auto, you will see the world spawning/de-spawning. It's not so bad at 21:9, but will be worse at wider resolutions. Adjust this value to your liking.");
+        Options.ToggleState = UseCustomMaxWidth.Value;
+        Options.OnToggleValueChanged += delegate(bool value) { UseCustomMaxWidth.Value = value; };
         UseCustomMaxWidth.SettingChanged += (_, _) =>
         {
             if (UseCustomMaxWidth.Value)
@@ -34,9 +79,12 @@ public class Plugin : BasePlugin
                 UpdatePugCameras();
             }
         };
-        CustomMaxWidth = Config.Bind("General", "MaxWidth", 480, "This is not the width in screen pixels. For example at 3440x1440 (21:9), a value of 630 expands the viewport entirely.");
+        CustomMaxWidth = Config.Bind("General", "MaxWidth", maxWidth, new ConfigDescription("This is not the width in screen pixels. For example at 3440x1440 (21:9), a value of 648 expands the viewport entirely.", new AcceptableValueRange<int>(480, maxWidth)));
+        Options.SliderValue = CustomMaxWidth.Value;
+        Options.OnSliderValueChanged += delegate(float value) { CustomMaxWidth.Value = Mathf.CeilToInt(value); };
         CustomMaxWidth.SettingChanged += (_, _) =>
         {
+            Logger.LogWarning($"CustomMaxWidth: {CustomMaxWidth.Value}");
             if (UseCustomMaxWidth.Value)
             {
                 UpdatePugCameras(true);
@@ -50,18 +98,27 @@ public class Plugin : BasePlugin
         SceneManager.sceneLoaded -= (UnityAction<Scene, LoadSceneMode>) SceneManagerOnSceneLoaded;
         return base.Unload();
     }
-
+    
     private static void UpdatePugCameras(bool custom = false)
     {
         var pugCameras = Resources.FindObjectsOfTypeAll<PugCamera>();
         foreach (var cam in pugCameras)
         {
-            cam.m_outputWidth = custom ? CustomMaxWidth.Value : cam.m_maxOutputWidth;
+            if (UseCustomMaxWidth.Value)
+            {
+                cam.m_outputMode = OutputMode.Fixed;
+                cam.m_outputWidth = CustomMaxWidth.Value;
+            }
+            else
+            {
+                cam.m_outputMode = OutputMode.MatchAspect;
+            }
             if (cam.camera != null)
             {
                 cam.camera.aspect = Display.main.systemWidth / (float) Display.main.systemHeight;
             }
         }
+        
     }
 
     private static void RemovePillars()
